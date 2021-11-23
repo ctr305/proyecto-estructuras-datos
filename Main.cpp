06c9dcf89fd71bf1a71a5f2fed0dd8bc1d1b57e0
sebasbello <85659138+sebasbello@users.noreply.github.com>
2021-11-22 21:02:06 -0600

    Add files via upload

---
+++ Main.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<stdio.h>
+#include<unistd.h>
+#include<cstdlib>
+#include<ctime>
+using namespace std;
+
+int main(){
+	
+	for(int i=0;i<30;i++){
+		system("cls");
+		cout<<i<<endl;
+		sleep(1);
+	}
+}
